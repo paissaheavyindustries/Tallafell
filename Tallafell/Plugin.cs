c6a309e1ad25ab496936ce40b5963829006da4a0
Paissa Heavy Industries <kommandantkeen@gmail.com>
2023-10-03 16:16:10 +0300

    1.0.0.1

--- Tallafell/Plugin.cs
+++ Tallafell/Plugin.cs
@@ -1,13 +1,11 @@
 ﻿using Dalamud.Game;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Objects.Types;
 using CharacterStruct = FFXIVClientStructs.FFXIV.Client.Game.Character.Character;
 using GameObjectStruct = FFXIVClientStructs.FFXIV.Client.Game.Object.GameObject;
-using Dalamud.Game.Gui;
-using Dalamud.Game.ClientState;
 using System;
+using Dalamud.Plugin.Services;
 
 namespace Tallafell
 {
@@ -18,19 +16,16 @@ namespace Tallafell
         public string Name => "Tallafell";
 
         private DalamudPluginInterface _pi { get; init; }
-        private ObjectTable _ot { get; init; }
-        private ChatGui _cg { get; init; }
-        private ClientState _cs { get; init; }
+        private IObjectTable _ot { get; init; }
+        private IChatGui _cg { get; init; }
+        private IClientState _cs { get; init; }
         private DateTime _nextCheck = DateTime.Now;
-
-        [PluginService]
-        public static SigScanner TargetModuleScanner { get; private set; }
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] ObjectTable objectTable,
-            [RequiredVersion("1.0")] ClientState clientState,
-            [RequiredVersion("1.0")] ChatGui chatGui
+            [RequiredVersion("1.0")] IObjectTable objectTable,
+            [RequiredVersion("1.0")] IClientState clientState,
+            [RequiredVersion("1.0")] IChatGui chatGui
         )
         {
             _pi = pluginInterface;
@@ -65,7 +60,7 @@ namespace Tallafell
                     {
                         CharacterStruct* bcs = (CharacterStruct*)bc.Address;
                         GameObjectStruct* gos = (GameObjectStruct*)go.Address;
-                        bcs->ModelScale = 2.0f;
+                        bcs->CharacterData.ModelScale = 2.0f;
                         gos->Scale = 2.0f;
                     }
                 }
